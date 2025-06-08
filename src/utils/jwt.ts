ccdd714552bc39a92bd6245a3c3270561304aeaa
sarodruiz <sarodruiz@gmail.com>
2025-06-08 01:57:55 -0300

    cicd test

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -1,6 +1,7 @@
 import jwt from 'jsonwebtoken';
 
 export const generateToken = (userId: string): string => {
+    console.log(`JWT_SECRET: ${process.env.JWT_SECRET}`); // Debugging line to check if JWT_SECRET is defined
     if (!process.env.JWT_SECRET) {
         throw new Error("JWT_SECRET is not defined");
     }
