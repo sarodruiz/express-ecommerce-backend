ea8043f43c17bb2cadf4889e021429f9c703690e
sarodruiz <sarodruiz@gmail.com>
2025-06-07 23:31:58 -0300

    added jwt and bcrypt dependencies, and modified scripts

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import connectDB from './config/database';
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT ?? 3000;
 
 const app = express();
 connectDB();
