88366440111b2d25db33811b031cb78898331e12
sarodruiz <sarodruiz@gmail.com>
2025-06-08 00:10:10 -0300

    added auth routes

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -3,43 +3,49 @@ import { Request, Response } from 'express';
 import { generateToken } from '../utils/jwt';
 import bcrypt from 'bcrypt';
 
-export const register = async (req: Request, res:Response) => {
+export const register = async (req: Request, res: Response): Promise<void> => {
     try {
         const { email, password } = req.body;
 
-        const existingUser = await User.find({ email });
-
+        const existingUser = await User.findOne({ email });
         if (existingUser) {
-            return res.status(400).json({ message: 'User already exists' });
+            res.status(400).json({ message: 'User already exists' });
+            return;
         }
 
         const hashedPassword = await bcrypt.hash(password, 10);
         const newUser = new User({ email, password: hashedPassword });
         await newUser.save();
-        res.status(201).json({ message: 'User registered successfully' });
-    } catch (error) {
-        res.status(500).json({ message: "Error creating user" });
-    }
-}
 
-export const login = async (req: Request, res: Response) => {
+        const token = generateToken(newUser._id.toString());
+
+        await res.status(201).json({ message: 'User registered successfully', token, user: { id: newUser._id, email: newUser.email } });
+    } catch (error) {
+        await res.status(500).json({ message: `Error creating user` });
+    }
+};
+
+
+export const login = async (req: Request, res: Response): Promise<void> => {
     try {
         const { email, password } = req.body;
 
         const user = await User.findOne({ email });
 
         if (!user) {
-            return res.status(400).json({ message: 'Invalid credentials' });
+            res.status(400).json({ message: 'Invalid credentials' });
+            return;
         }
 
         const isPasswordValid = await bcrypt.compare(password, user.password);
 
         if (!isPasswordValid) {
-            return res.status(400).json({ message: 'Invalid credentials' });
+            res.status(400).json({ message: 'Invalid credentials' });
+            return;
         }
 
         const token = generateToken(user._id.toString());
-        
+
         res.status(200).json({ token, user: { id: user._id, email: user.email } });
     } catch (error) {
         res.status(500).json({ message: "Error logging in user" });
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import connectDB from './config/database';
+import authRoutes from './routes/auth.routes';
 
 const PORT = process.env.PORT ?? 3000;
 
@@ -7,9 +8,12 @@ const app = express();
 app.use(express.json());
 connectDB();
 
+app.use('/api/auth', authRoutes);
+
 app.get('/', (req, res) => {
   res.send('Welcome to my Ecommerce API!');
 });
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
